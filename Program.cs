fc3079e7d9ca6d9907d3a57c46f6648e7882b868
azzure277 <brandon.govender21@outlook.com>
2025-09-24 06:50:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mssa_12._3._1
+{
+    internal class Program
+    {
+        public static string RemoveDuplicates(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (stack.Count > 0 && stack.Peek() == c)
+                    stack.Pop();
+                else
+                    stack.Push(c);
+            }
+            return new string(stack.Reverse().ToArray());
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "abbaca";
+            string result = RemoveDuplicates(s);
+            Console.WriteLine(result); // Output: "ca"
+        }
+    }
+}
